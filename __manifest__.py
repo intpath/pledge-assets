f72823d0f67fa68733e3a32a2fc8d3ab2a1b4926
M-Saeb <mohammed.saeb98@hotmail.com>
2021-03-02 11:25:33 +0000

    added user group

--- __manifest__.py
+++ __manifest__.py
@@ -3,38 +3,29 @@
     'name': "Pledges",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        For Creating & Managing Pledges""",
 
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Inteferated Path",
+    'website': "https://www.int-path.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '1',
+    'application':True,
 
     # any module necessary for this one to work correctly
     'depends': ['base','mail','contacts'],
 
     # always loaded
     'data': [
+        'security/res_groups_data.xml',
         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
         'views/pledge_pledge_views.xml',
         'views/pledge_bank_views.xml',
         'views/res_partner_view.xml',
         'views/res_config_view.xml'
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
-    'application':True,
 }
